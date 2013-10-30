cf1489bf2024c0d607efb247e9668a7123f44db7
john <john@localhost>
2013-10-30 23:25:44 +0000

    Use forEach instead of for loop on array in cell_changed

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@
 
 //--- CODE --------------------------
 
-function assert(condition, message) {
-    if (!condition) {
-        console.log(message);
-        //alert(message);
-    }
+function assert (condition, message) {
+  if (!condition) {
+    console.log(message);
+    //alert(message);
+  }
 }
 
 
-function trace(message) {
+function trace (message) {
   console.log(message);
 }
 
@@ -85,7 +85,7 @@ Cell.prototype.remove_candidate = function (value) {
   if (this.num_candidates === 1) {
     var must_be_value = this.get_only_remaining_candidate();
     console.log("value must be " + must_be_value);
-    
+
     // DANGER: recursion
     this.set_value(must_be_value);
   }
@@ -93,11 +93,12 @@ Cell.prototype.remove_candidate = function (value) {
 
 Cell.prototype.set_value = function (new_value) {
   assert(this.value === null, "attempt to set cell value after already set");
-  assert(this.value_is_a_candidate(new_value), "attempt to set cell to a value which is not a candidate");
-  
+  assert(this.value_is_a_candidate(new_value),
+          "attempt to set cell to a value which is not a candidate");
+
   this.value = new_value;
   this.remove_all_candidates();
-  
+
   var grps = this.constraint_groups;
   assert(grps.length > 0, "cell has no constraint groups");
   for (var i = 0; i < grps.length; i++) {
@@ -112,23 +113,31 @@ var ConstraintGroup = function (cells_array) { // Constructor
   this.cells = cells_array;
   // hook cells up to constraint group
   for (var i = 0; i < cells_array.length; i++) {
-    
+
     cells_array[i].add_constraint_group(this);
-    
+
   }
 };
 
-ConstraintGroup.prototype.cell_changed = function (cell, new_value) {
+ConstraintGroup.prototype.cell_changed = function (changed_cell, new_value) {
   // Apply constraint for all other cells in this group.
-  
+
   // For Sudoku, when a cell value has been set, we have to remove
   // the value from the candidate list of all other cells in this
   // constraint group.
+  
+  /*
   for (var i = 0; i < this.cells.length; i++) {
-    if (cell !== this.cells[i]) {
+    if (changed_cell !== this.cells[i]) {
       this.cells[i].remove_candidate(new_value);
     }
-  }
+  }*/
+  
+  this.cells.forEach(function(iterator_cell) {
+    if (iterator_cell !== changed_cell) {
+      iterator_cell.remove_candidate(new_value);
+    }
+  });
   // TODO:
   // Check to see if any candidate appears only once.
 };
@@ -140,28 +149,28 @@ ConstraintGroup.prototype.cell_changed = function (cell, new_value) {
 
 describe("Candidates tests", function () {
   var cand;
-  
+
   beforeEach(function () {
     cand = new Candidates([100, 200, 300, 400, 500]);
   });
-  
+
   it("count_candidates()).toBe(5)", function () {
     expect(cand.count_candidates()).toBe(5);
   });
-  
+
   it("value_is_a_candidate(200)).toBe(true)", function () {
     expect(cand.value_is_a_candidate(200)).toBe(true);
   });
-  
+
   it("value_is_a_candidate(600)).toBe(false)", function () {
     expect(cand.value_is_a_candidate(600)).toBe(false);
   });
-  
+
   it("value_is_a_candidate(200)).toBe(false) after remove", function () {
     cand.remove_candidate(200);
     expect(cand.value_is_a_candidate(200)).toBe(false);
   });
-  
+
   it("count_candidates().toBe(4) after remove", function () {
     cand.remove_candidate(500);
     expect(cand.count_candidates()).toBe(4);
@@ -170,23 +179,23 @@ describe("Candidates tests", function () {
 
 describe("Cell tests", function () {
   var cell;
-  
+
   beforeEach(function () {
     cell = new Cell([100, 200, 300, 400, 500]);
   });
-  
+
   it("value_is_a_candidate(200)).toBe(true)", function () {
     expect(cell.value_is_a_candidate(200)).toBe(true);
   });
-  
+
   it("value_is_a_candidate(600)).toBe(false)", function () {
     expect(cell.value_is_a_candidate(600)).toBe(false);
   });
-  
+
   it("get_only_remaining_candidate()).toBe(null)", function () {
     expect(cell.get_only_remaining_candidate()).toBe(null);
   });
-  
+
   it("get_value()).toBe(300)", function () {
     cell.remove_candidate(100);
     cell.remove_candidate(200);
@@ -194,7 +203,7 @@ describe("Cell tests", function () {
     cell.remove_candidate(500);
     expect(cell.get_value()).toBe(300);
   });
-  
+
   it("value_is_a_candidate(200)).toBe(false) after remove", function () {
     cell.remove_candidate(200);
     expect(cell.value_is_a_candidate(200)).toBe(false);
@@ -211,25 +220,25 @@ describe("Candidate tests", function () {
     grp1 = new ConstraintGroup([cell1, cell2]);
     grp2 = new ConstraintGroup([cell2, cell3]);
   });
-  
+
   it("cell1.get_value().toBe(1) after cell1.set_value(1)", function () {
     cell1.set_value(1);
     expect(cell1.get_value(1)).toBe(1);
   });
-  
+
   it("cell2.value_is_a_candidate(1)).toBe(false)", function () {
     cell1.set_value(1);
     expect(cell2.value_is_a_candidate(1)).toBe(false);
   });
-  
+
   it("cell2.get_value()).toBe(2)", function () {
     cell1.set_value(1);
     expect(cell2.get_value()).toBe(2);
   });
-  
+
   it("cell3.get_value()).toBe(1)", function () {
     cell1.set_value(1);
     expect(cell3.get_value()).toBe(1);
   });
-  
+
 });
