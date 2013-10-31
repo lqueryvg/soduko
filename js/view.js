1499718592388ca51347c618cb0b65df128ef9aa
John <John@John-PC>
2013-10-31 16:51:31 +0000

    working on the view and more restructure

--- js/view.js
+++ js/view.js
@@ -4,4 +4,24 @@
  * and open the template in the editor.
  */
 
+$(document).ready(function() {
 
+  "use strict";
+  //var data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+  var html = '<table>', cell_name;
+
+  [1, 2, 3].forEach(function(row) {
+    html += '<tr>';
+    [1, 2, 3].forEach(function(col) {
+      var block_name = "b" + row.toString() + col.toString();
+      html += '<td id="' + block_name + '">' + block_name + '</td>';
+    });
+    html += '</tr>';
+  });
+
+  html += '</table>';
+
+  $(html).appendTo('body');
+
+});
